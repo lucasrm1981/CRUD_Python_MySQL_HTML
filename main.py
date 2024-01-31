9ca55bdd80c86d3a3637b9a3185a54b12842dfa9
lucasrm1981 <lucas@lksistemas.com.br>
2024-01-31 14:38:44 -0300

    Criação do Projeto

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
-import mysql.connector ## Importação para conectar ao mysql
-import random
+import mysql.connector  ## Importação para conectar ao mysql
+import random  ## Biblioteca para número randomico
+
+## Conexão com a tabela criada
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="db_python"
+)
+mycursor = mydb.cursor()
+
 
 def create_db():
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Root@123")
+        password="")
     mycursor = mydb.cursor()
     mycursor.execute("CREATE DATABASE db_python")
 
-def create_tables():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Root@123",
-        database="db_python"
-    )
-## Nome da base criada
-    mytable = mydb.cursor()
-    mytable .execute("CREATE TABLE students ("
+
+def create_table():
+    mycursor.execute("CREATE TABLE students ("
                      "id_aluno INTEGER primary key auto_increment NOT NULL, "
-                     "matricula INTEGER, "
+                     "matricula VARCHAR(20), "
                      "nome VARCHAR(100), "
                      "sobrenome VARCHAR(100), "
                      "idade INTEGER,"
                      "email VARCHAR(255),"
-                     "rf FLOAT," ## Renda percápita
-                     "fil VARCHAR(255)," ## Filiação
+                     "rf FLOAT,"  ## Renda percápita
+                     "fil VARCHAR(255),"  ## Filiação
                      "cpf VARCHAR(14),"
-                     "esc VARCHAR(20)," ## Escolaridade
-                     "enem_n INTEGER" ## Nota do ENEM
+                     "esc VARCHAR(20),"  ## Escolaridade
+                     "enem_n INTEGER"  ## Nota do ENEM
                      ")")
 
-def create_students():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Root@123",
-        database="db_python"
-    )
 
-    mycursor = mydb.cursor()
-
-    matricula = random.randint(10000, 90000)
+def create_student():
     nome = input("Digite seu Nome: ")
     sobrenome = input("Digite seu Sobrenome: ")
     idade = int(input("Digite sua Idade: "))
@@ -56,25 +50,27 @@ def create_students():
     else:
         enem_n = 0
 
-    sql = (f"INSERT INTO students (matricula,nome, sobrenome,idade,email,rf,fil,cpf,esc,enem_n) "
-           f"VALUES ({matricula}, '{nome}','{sobrenome}',{idade},'{email}',"
+    sql = (f"INSERT INTO students (nome, sobrenome,idade,email,rf,fil,cpf,esc,enem_n) "
+           f"VALUES ('{nome}','{sobrenome}',{idade},'{email}',"
            f"{rf},'{fil}','{cpf}','{esc}',{enem_n})")
 
     mycursor.execute(sql)
     mydb.commit()
+
+    last_id = mycursor.lastrowid  ## retorna o ultimo id
+    matricula = str(random.randint(10000, 90000)) + "-" + str(last_id)
+    cad_mat = f"UPDATE students SET matricula='{matricula}' WHERE id_aluno={last_id}"
+    mycursor.execute(cad_mat)
+    mydb.commit()
+
     print(mycursor.rowcount, "Registro Inserido.")  ## IMPRESSÃO DA QUANTIDADE DE LINHAS INSERIDAS
 
-def show_students():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Root@123",
-        database="db_python"
-    )
 
-    mycursor = mydb.cursor()
-    campoBusca = input("Deseja procurar por qual campo? ou * para Visualizar Todos Alunos")
-    valorBusca = input("Digite o valor da Busca")
+def show_students():
+    campoBusca = input("Deseja procurar por qual campo? ou * para Visualizar Todos Alunos: \n")
+    if campoBusca != "*":
+        valorBusca = input("Digite o valor da Busca: \n")
+
     ## LIKE %TEXTO ignora a parte inicial e completa com o valor digitado
     ## TEXTO% ignora a parte final
     if campoBusca == "*":
@@ -83,20 +79,31 @@ def show_students():
         query = f"SELECT * FROM students WHERE {campoBusca} LIKE '%{valorBusca}%'"
 
     mycursor.execute(query)
-
     myresult = mycursor.fetchall()
     ## Imprime como uma Lista Linha a Linha
     for x in myresult:
         print(x)
 
-def delete_student():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Root@123",
-        database="db_python")
 
-    mycursor = mydb.cursor()
+def update_student():
+    matricula = input("Digite a matrícula do Estudante que deseja alterar:\n ")
+    nomeColuna = input("Deseja atualizar qual campo?\n")
+    if nomeColuna == "matricula":
+        print("Não é possível alterar a Matrícula")
+        update_student()
+    novoValor = input("Digite o novo valor do campo\n")
+
+    query = f"UPDATE students SET {nomeColuna} = '{novoValor}' WHERE matricula= '{matricula}'"
+
+    mycursor.execute(query)
+    mydb.commit()
+    if mycursor.rowcount > 0:
+        print(mycursor.rowcount, "Registro(s) Atualizados.")
+    else:
+        print("Nenhum Resultado encontrado")
+
+
+def delete_student():
     campoBusca = input("Deseja procurar por qual campo para Deletar?")
     valorBusca = input("Digite o valor da Busca")
     ## LIKE %TEXTO ignora a parte inicial e completa com o valor digitado
@@ -109,29 +116,33 @@ def delete_student():
     else:
         print("Nenhum Resultado encontrado")
 
+
 opcao = str.lower(input("Deseja utilizar o sistema de Cadastro de Alunos? S ou N\n"))
-while opcao=="s":
+while opcao == "s":
     menu = input("Escolha a opção desejada:\n"
                  "1. Criar Base de Dados\n"
                  "2. Criar tabela dos Alunos\n"
                  "3. Inserir Aluno\n"
                  "4. Deletar Aluno\n"
-                 "5. Listar Alunos\n"
-                 "6. Sair do Programa\n")
+                 "5. Atualizar Informação\n"
+                 "6. Listar Aluno(s)\n"
+                 "7. Sair do Programa\n")
     match menu:
         case '1':
             create_db()
         case '2':
-            create_tables()
+            create_table()
         case '3':
-            create_students()
+            create_student()
         case '4':
             delete_student()
         case '5':
-            show_students()
+            update_student()
         case '6':
+            show_students()
+        case '7':
             exit(0)
         case _:
-            print("Opção não Encontrada escolha de 1 até 5 ")
+            print("Opção não Encontrada escolha de 1 até 7 ")
 
     opcao = str.lower(input("Deseja utilizar o sistema de Cadastro de Alunos? S ou N\n"))
